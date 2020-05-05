b9d63851dd671f5639b45e2c719d404aacad2350
Jason Hinton <jason_hinton@att.net>
2020-05-04 21:40:15 -0400

    added a few pics

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,29 @@
 import React from "react";
 
-
 const Home = () => {
+	return (
+		<div className="home">
+			<img
+				alt="pizza"
+				src="https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"></img>
+			
+				<h1>Get Your Munch On!!!</h1>
 
-  return(
-	<div className="home">
-		<img alt='free delivery' src='https://images.unsplash.com/photo-1511495366194-31561af5b3d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'></img>
-    <img alt='pizza' src='https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'></img>
-	</div>
-)};
+			<img
+				alt="free delivery"
+				src="https://images.unsplash.com/photo-1511495366194-31561af5b3d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"></img>
 
+				<img alt= 'pizza hut' src='https://images.unsplash.com/photo-1584190926897-0023f6aa05f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'></img>
+
+
+				<img alt= 'neon sign' src= 'https://images.unsplash.com/photo-1518893494013-481c1d8ed3fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'></img>
+
+				<img alt= 'eat sign' src='https://images.unsplash.com/photo-1527224538127-2104bb71c51b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'></img>
+
+				<img alt= 'pizza restaurant' src= 'https://images.unsplash.com/photo-1532115126980-8729c054bc77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'></img>
+		</div>
+		
+			
+	)};
 
 export default Home;
