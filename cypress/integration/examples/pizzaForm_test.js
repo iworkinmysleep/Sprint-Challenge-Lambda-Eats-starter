c1345b74447bdeb81633887d8965ceabfeedcfc9
Jason Hinton <jason_hinton@att.net>
2020-05-04 20:32:00 -0400

    mvp

--- cypress/integration/examples/pizzaForm_test.js
+++ cypress/integration/examples/pizzaForm_test.js
@@ -2,7 +2,7 @@
 
 describe("Testing our form inputs", function () {
 	beforeEach(function () {
-		cy.visit("http://localhost:3000/");
+		cy.visit("http://localhost:3000/pizza");
 	});
 	it("adds text to box", function () {
 		cy.get('[data-cy="name"]').type("Jason").should("have.value", "Jason");
